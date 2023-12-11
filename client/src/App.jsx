a0c6a1cc7e3f18508767d22414c4c1084190c0f1
Solomon-03 <solomon172003@gmail.com>
2023-12-11 12:05:37 +0530

    Pages & Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import {BrowerRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Booking from './pages/Booking';
+import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import React from 'react';
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowerRouter>
+    <Header />
+    <Routes>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='booking' element={<Booking/>}/>
+      <Route path='sign-in' element={<SignIn/>}/>
+      <Route path='sign-up' element={<SignUp/>}/>
+    </Routes>
+    </BrowerRouter>
   )
 }
 
