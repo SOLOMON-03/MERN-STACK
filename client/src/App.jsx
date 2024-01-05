e16fb13e8a30f12d09e35d6b8844c691d2c78756
Solomon-03 <solomon172003@gmail.com>
2024-01-05 17:29:50 +0530

    fix signin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Search from './pages/Search';
-import SignIn from './pages/Signin';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import React from 'react';
